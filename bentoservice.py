d0570b72162f2ea34a3f82c5816bf0ab282a7a29
buslovaev_ma <buslovaev_ma@outlook.com>
2023-11-01 14:15:29 +0000

    added bentoml test

---
+++ bentoservice.py
@@ -0,0 +1,20 @@
+import bentoml
+from bentoml.adapters import JsonInput
+from bentoml.adapters import DataframeInput
+from bentoml.frameworks.sklearn import SklearnModelArtifact
+from sinara.bentoml_artifacts.binary_artifact import BinaryFileArtifact
+
+@bentoml.env(infer_pip_packages=True)
+@bentoml.artifacts([
+    SklearnModelArtifact('model'),
+    BinaryFileArtifact('some_model_binary_artifact')
+])
+
+class TestService(bentoml.BentoService):
+    """
+    TestService is designed for...
+    """
+
+    @bentoml.api(input=DataframeInput(), batch=True)
+    def predict(self, df):
+        return self.artifacts.model.predict(df)
