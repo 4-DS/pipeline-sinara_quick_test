dc1037ab4a990adbce1d52eceb8dadffc98d07c3
buslovaev_ma <buslovaev_ma@outlook.com>
2023-11-19 07:54:42 +0000

    test changes

--- test_step.py
+++ test_step.py
@@ -3,8 +3,8 @@ from sinara.step import StepSafeguard as sg
 from sinara.step import StepReport as sr
 
 sg.step_is_in_dir("**/work/**")
-sg.step_is_in_branch(["main","ver*"])
-sg.git_reset()
+#sg.step_is_in_branch(["main","ver*"])
+#sg.git_reset()
 
 try:
     step = Step(step_params_file_globs="params/test_step_params.json",
