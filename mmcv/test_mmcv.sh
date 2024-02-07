92e90ba816e5de04e1313f6df8377f682cb11254
ExcaliburKG <akagorbunov@ya.ru>
2024-02-07 12:49:32 +0000

    Updated mmcv test

--- mmcv/test_mmcv.sh
+++ mmcv/test_mmcv.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-rm -rf /home/jovyan/work/mmdetection
-ln -s /home/jovyan/mmdetection /home/jovyan/work/mmdetection
-
 cd /home/jovyan/work/mmdetection
 python demo/image_demo.py demo/demo.jpg rtmdet-s
-python demo/image_demo.py demo/demo.jpg rtmdet-ins-s --show
+python demo/image_demo.py demo/demo.jpg rtmdet-ins-s
